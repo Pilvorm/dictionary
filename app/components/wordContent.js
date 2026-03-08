b459593cb0715e385dffdbca0502dc5a360452af
DANNNN <danielemerald@ymail.com>
2026-03-08 20:51:44 +0700

    Update wordContent.js

--- app/components/wordContent.js
+++ app/components/wordContent.js
@@ -1,6 +1,5 @@
 import Pronounciation from "../components/pronounciation";
 import Source from "../components/source";
-import Loading from "../[word]/loading";
 
 const WordContent = async ({ word }) => {
   const res = await fetch(
@@ -54,8 +53,6 @@ const WordContent = async ({ word }) => {
         phonetic={entry.phonetic}
         audio={audio}
       />
-
-      <Loading />
 
       {entry.meanings.map((meaning, index) => (
         <section key={index} className="flex flex-col gap-10">
